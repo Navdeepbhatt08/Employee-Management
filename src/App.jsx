a7cf312df64311ab3f6bcc445b3f6a82108e4caa
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2025-03-20 14:57:18 +0530

    User Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,37 @@ import React, { useState, useEffect, useContext } from "react";
 import Login from "./components/auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 import { AuthContext } from "./context/AuthProvider";
 
 const app = () => {
 
   const [user, setUser] = useState(null)
   const authData = useContext(AuthContext)
- 
+
+  useEffect(() => {
+  if(authData){
+    const  loggedInUser = localStorage.getItem('loggedInUser ')
+      if(loggedInUser)
+      {
+        setUser(loggedInUser.role)
+      }
+  }
+  
+  }, )
+  
 
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
+       
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
+
     }
-    else if (email == 'user@me.com' && password == '123') {
+    else if (authData && authData.employees.find((e)=> email == e.email && e.password ==password)) {
       setUser('employee')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
+
 
     }
     else {
