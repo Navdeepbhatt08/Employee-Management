95ca5fbe026fbd521491d3631e3255ed04a87843
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2025-03-22 13:52:43 +0530

    Added Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,31 @@ const app = () => {
   const [user, setUser] = useState(null)
   const authData = useContext(AuthContext)
 
-  useEffect(() => {
-  if(authData){
-    const  loggedInUser = localStorage.getItem('loggedInUser ')
-      if(loggedInUser)
-      {
-        setUser(loggedInUser.role)
-      }
-  }
-  
-  }, )
-  
+  // useEffect(() => {
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem('loggedInUser ')
+  //     if (loggedInUser) {
+  //       setUser(loggedInUser.role)
+  //     }
+  //   }
+  // },[authData]);
 
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
-      setUser('admin')
-       
-      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
+    setUser({role:'admin'})
+
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
 
     }
-    else if (authData && authData.employees.find((e)=> email == e.email && e.password ==password)) {
+    else if (authData) {
+       
+           const employee =  authData.employees.find((e) => email == e.email && e.password == password);
+           if(employee){
+            setUser({role:employee})
+           }
       setUser('employee')
-      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }))
 
 
     }
